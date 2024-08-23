bfd5225455c094f77c54500674eded273b41c408
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-08-23 13:35:30 +0200

    add: client function handles for the rest of the function types

--- src/client.rs
+++ src/client.rs
@@ -3,12 +3,12 @@ use std::{env, fs::File, io::Read};
 
 use stat_service::stat_methods_client::StatMethodsClient;
 use stat_service::{
-    Empty, NumberOfCitiesRequest, NumberOfCitiesResponse, NumberOfCountriesMaxRequest,
+    NumberOfCitiesRequest, NumberOfCitiesResponse, NumberOfCountriesMaxRequest,
     NumberOfCountriesMaxResponse, NumberOfCountriesRequest, NumberOfCountriesResponse,
-    PopulationRequest, PopulationResponse, RecordsResponse,
+    PopulationRequest, PopulationResponse,
 };
 use tonic::metadata::MetadataValue;
-use tonic::Response;
+use tonic::{Request, Response};
 
 pub mod stat_service {
     tonic::include_proto!("statservice");
@@ -37,7 +37,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("[INFO] Client read file: {}", file_path);
 
     // Connect to the server
-    let addr = "127.0.0.1:50051";
+    let addr = "http://127.0.0.1:50051";
     let mut client: StatMethodsClient<tonic::transport::Channel> =
         StatMethodsClient::connect(addr).await?;
 
@@ -59,7 +59,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 let zone = inputs[2].parse::<i32>()?;
 
                 // Build the request to the server
-                let mut request = tonic::Request::new(PopulationRequest {
+                let mut request = Request::new(PopulationRequest {
                     country: country_name.to_string(),
                 });
 
@@ -82,6 +82,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 // Calculate the duration
                 let turnaround_time = start.elapsed();
 
+                // TODO: Get execution time from the server as metadata 
+
                 let population: i32 = response.get_ref().population;
 
                 // Print the result
@@ -89,14 +91,119 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 XX ms, waiting time: XX ms, processed by Server 1)", country_name, zone, population, turnaround_time.as_millis())
             }
             "getNumberofCities" => {
-                todo!()
+                assert!(inputs.len() == 4);
+                let country_name = inputs[1];
+                let min = inputs[2].parse::<i32>()?;
+                let zone = inputs[3].parse::<i32>()?;
+
+                // Build the request to the server
+                let mut request = Request::new(NumberOfCitiesRequest {
+                    country: country_name.to_string(),
+                    min, 
+                });
+
+                // Set zone data as meta data in the request
+                request
+                    .metadata_mut()
+                    .insert("client_zone", MetadataValue::from(client_zone));
+
+                request
+                    .metadata_mut()
+                    .insert("request_zone", MetadataValue::from(zone));
+
+                // Capture the start time
+                let start = Instant::now();
+
+                // Get the response
+                let response: Response<NumberOfCitiesResponse> = client.get_number_of_cities(request).await?;
+
+                // Calculate the duration
+                let turnaround_time = start.elapsed();
+
+                // TODO: Get execution time from the server as metadata 
+
+                let number_of_cities: i32 = response.get_ref().number_of_cities;
+
+                // Print the result
+                println!("[INFO] getNumberofCities for {} min: {}, Number of cities: {}, (turnaround time: {} ms, execution time:
+XX ms, waiting time: XX ms, processed by Server 1)", country_name, min, number_of_cities, turnaround_time.as_millis())
             }
             "getNumberofCountries" => {
-                todo!()
+                assert!(inputs.len() == 4);
+                let citycount = inputs[1].parse::<i32>()?;
+                let min = inputs[2].parse::<i32>()?;
+                let zone = inputs[3].parse::<i32>()?;
+                
+                let mut request = Request::new(NumberOfCountriesRequest {
+                    citycount,
+                    min
+                });
+
+                // Set zone data as meta data in the request
+                request
+                    .metadata_mut()
+                    .insert("client_zone", MetadataValue::from(client_zone));
+
+                request
+                    .metadata_mut()
+                    .insert("request_zone", MetadataValue::from(zone));
+
+                // Capture the start time
+                let start = Instant::now();
+
+                // Get the response
+                let response: Response<NumberOfCountriesResponse> = client.get_number_of_countries(request).await?;
+
+                // Calculate the duration
+                let turnaround_time = start.elapsed();
+
+                // TODO: Get execution time from the server as metadata 
+
+                let result: i32 = response.get_ref().result;
+
+                // Print the result
+                println!("[INFO] getNumberofCountries with citycount: {} min: {}, Result: {}, (turnaround time: {} ms, execution time:
+XX ms, waiting time: XX ms, processed by Server 1)", citycount, min, result, turnaround_time.as_millis())
             }
             "getNumberofCountriesMax" => {
-                todo!()
-            }
+                assert!(inputs.len() == 5);
+                let citycount = inputs[1].parse::<i32>()?;
+                let min = inputs[2].parse::<i32>()?;
+                let max = inputs[3].parse::<i32>()?;
+                let zone = inputs[4].parse::<i32>()?;
+                
+                let mut request = Request::new(NumberOfCountriesMaxRequest {
+                    citycount,
+                    min,
+                    max
+                });
+
+                // Set zone data as meta data in the request
+                request
+                    .metadata_mut()
+                    .insert("client_zone", MetadataValue::from(client_zone));
+
+                request
+                    .metadata_mut()
+                    .insert("request_zone", MetadataValue::from(zone));
+
+                // Capture the start time
+                let start = Instant::now();
+
+                // Get the response
+                let response: Response<NumberOfCountriesMaxResponse> = client.get_number_of_countries_max(request).await?;
+
+                // Calculate the duration
+                let turnaround_time = start.elapsed();
+
+                // TODO: Get execution time from the server as metadata 
+
+                let result: i32 = response.get_ref().result;
+
+                // Print the result
+                println!("[INFO] getNumberofCountries with citycount: {} min: {}, max: {} Result: {}, (turnaround time: {} ms, execution time:
+XX ms, waiting time: XX ms, processed by Server 1)", citycount, min, max, result, turnaround_time.as_millis())
+            } 
             unknown => {
                 println!("[ERROR] Unknown function name: {unknown}")
             }
